96a109a615bba5cc34ee4aebec6fad43465ff632
HUNG3 NGUYEN VAN <nvhung3@NVHUNG3-VDI>
2022-11-23 15:36:11 +0700

    create app

--- ACM_Console/Program.cs
+++ ACM_Console/Program.cs
@@ -7,23 +7,7 @@ namespace ACM_Console
     {
         static void Main(string[] args)
         {
-            using (var context = new ACMContext())
-            {
-                var tenant = new User() {  };
-                context.Tenants.Add(tenant);
-                context.SaveChanges();
-            }
-
-            Console.WriteLine("after Update data");
-
-            using (var context = new ACMContext())
-            {
-                var tenants = context.Tenants;
-                foreach(var item in tenants)
-                {
-                    Console.WriteLine(item.TenantName);
-                }
-            }
+            
         }
     }
 }
